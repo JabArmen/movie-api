25478cf041d0da34359c719bd691037c7d3e3cd3
JabArmen <armenjamgotchian7@gmail.com>
2022-11-28 00:53:10 -0500

    added composite movies

--- README.md
+++
@@ -1,2 +0,0 @@
-# movie-api
-A simple API that has resources such as Movies, Shows, Actors, Characters, Reviews, Directors and Studios
---
+++ includes/helpers/composite.php
@@ -0,0 +1,53 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+//var_dump($_SERVER["REQUEST_METHOD"]);
+use Slim\Factory\AppFactory;
+
+require_once __DIR__ . './../models/BaseModel.php';
+require_once __DIR__ . './../models/MovieModel.php';
+
+use GuzzleHttp\Client;
+
+$movie_model = new MovieModel();
+if (count($movie_model->getAll()['data']) == 0) {
+    $client = new GuzzleHttp\Client();
+
+    $uri_api = "https://api.themoviedb.org/3/movie/550?api_key=3d95b2e2eb80aade533ec63b88c8f998";
+    $response = $client->get($uri_api);
+    $data = $response->getBody()->getContents();
+    $movie = json_decode($data, true);
+
+    $new_movie = array(
+        "movie_id" => 1,
+        "title" => $movie["original_title"],
+        "poster" => $movie["poster_path"],
+        "release_date" => $movie["release_date"],
+        "budget" => $movie["budget"],
+        "release_date" => $movie["release_date"],
+        "genre" => $movie["genres"][0]["name"]
+    );
+    $movie_model->createMovie($new_movie);
+
+
+    $uri_api = "https://api.themoviedb.org/3/movie/438148?api_key=3d95b2e2eb80aade533ec63b88c8f998";
+    $response = $client->get($uri_api);
+    $data = $response->getBody()->getContents();
+    $movie = json_decode($data, true);
+
+    $new_movie = array(
+        "movie_id" => 2,
+        "title" => $movie["original_title"],
+        "poster" => $movie["poster_path"],
+        "release_date" => $movie["release_date"],
+        "budget" => $movie["budget"],
+        "release_date" => $movie["release_date"],
+        "genre" => $movie["genres"][0]["name"],
+        "director_id" => 1,
+        "studio_id" => 1
+    );
+    $movie_model->createMovie($new_movie);
+
+    
+}
